6744c51e3133e0fb16d51be4e64ca22e20461c8a
NestorAndres1215 <atiroandres@gmail.com>
2026-06-22 23:37:24 -0500

    subiendo proyecto

---
+++ backend/src/test/java/com/san_andres/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san_andres.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
