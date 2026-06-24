73f70643dff2d735e028d8b5076eb110d972dc9e
NestorAndres1215 <atiroandres@gmail.com>
2026-06-24 17:33:35 -0500

    subiendo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Principal } from './features/principal/principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: Principal,
+        pathMatch: 'full'
+    },
+
+];
