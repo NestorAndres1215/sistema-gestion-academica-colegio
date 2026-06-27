55f22d59b4b0de87f04ef31ea0dc2a28f354f85c
NestorAndres1215 <atiroandres@gmail.com>
2026-06-27 09:35:31 -0500

    subiendo

--- frontend/src/app/features/admin/admin-main/admin-main.ts
+++ frontend/src/app/features/admin/admin-main/admin-main.ts
@@ -1,47 +1,42 @@
 import { Component } from '@angular/core';
-import { BreadCrumb, BreadcrumbItem } from '../../../shared/ui/bread-crumb/bread-crumb';
 import { CommonModule } from '@angular/common';
-import { SearchBar } from "../../../shared/ui/search-bar/search-bar";
-import { Pagination } from "../../../shared/ui/pagination/pagination";
-import { PageTitle } from "../../../shared/ui/page-title/page-title";
-import { Button } from "../../../shared/ui/button/button";
+
+import { BreadCrumb, BreadcrumbItem } from '../../../shared/ui/bread-crumb/bread-crumb';
+import { SearchBar } from '../../../shared/ui/search-bar/search-bar';
+import { Pagination } from '../../../shared/ui/pagination/pagination';
+import { PageTitle } from '../../../shared/ui/page-title/page-title';
+import { Button } from '../../../shared/ui/button/button';
+import { DataTable } from '../../../shared/ui/data-table/data-table';
 
 @Component({
   selector: 'app-admin-main',
   standalone: true,
-  imports: [CommonModule, BreadCrumb, SearchBar, Pagination, PageTitle, Button],
+  imports: [
+    CommonModule,
+    BreadCrumb,
+    SearchBar,
+    
+    PageTitle,
+    Button,
+    
+  ],
   templateUrl: './admin-main.html'
 })
 export class AdminMain {
-filter($event: string) {
-throw new Error('Method not implemented.');
-}
-  page = 1;
-  totalPages = 10;
 
-  onPageChange(page: number) {
-    this.page = page;
-    // Cargar datos de la página
-  }
   breadcrumbs: BreadcrumbItem[] = [
     {
       label: 'Dashboard',
       href: '/admin'
     },
     {
-      label: 'Usuarios',
-      href: '/admin/users'
-    },    {
-      label: 'Dashboard',
-      href: '/admin'
-    },
-    {
-      label: 'Usuarios',
-      href: '/admin/users'
-    },
-    {
-      label: 'Nuevo Usuario'
+      label: 'Usuarios'
     }
   ];
+
+  page = 1;
+  pageSize = 5;
+
+ 
 
 }
